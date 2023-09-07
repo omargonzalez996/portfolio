7e634c75567410bbd08088d5e466efe65b757d7f
omargonzalez996 <omar.gonz996@gmail.com>
2023-09-07 14:53:24 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.png'
+function App() {
+  return (
+    <div className="up-cont">
+      <div className="name-title">
+        {/* <img src={logo} alt="logo.svg" width={50} height={50} /> */}
+        <h2>Omar Gonzalez</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
